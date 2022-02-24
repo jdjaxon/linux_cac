4a13910253dd0aa7469f55efcf8f63ce5fa41684
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2022-02-24 16:30:25 -0500

    Adding current progress.

--- cac_setup.sh
+++ cac_setup.sh
@@ -5,12 +5,11 @@
 # Author: Jeremy Jackson
 # Date: 24 Feb 2022
 # Description: Setup a Linux environment for Common Access Card use.
-# References:
-#   1. https://tldp.org/LDP/abs/html/sha-bang.html
 
 DWNLD_DIR=/tmp
 ROOT_UID=0      # Only users with $UID 0 have root privileges
-E_NOTROOT=87    # Non-root exit error
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
 CERT_FILENAME="AllCerts"
 BUNDLE_FILENAME="AllCerts.zip"
 CERT_URL="http://militarycac.com/maccerts/$BUNDLE_FILENAME"
@@ -42,6 +41,20 @@ echo -e "Done\n"
 mkdir -p $DWNLD_DIR/$CERT_FILENAME
 unzip $DWNLD_DIR/$BUNDLE_FILENAME -d $DWNLD_DIR/$CERT_FILENAME
 
+# Check for Chrome
+_=google-chrome --version
+if [ $? -eq 0 ]
+then
+    # TODO do chrome loop here
+fi
+
+# Check for Firefox
+_=firefox --version
+if [ $? -eq 0 ]
+then
+    # TODO do firefox loop here
+fi
+
 # Removed artifacts
 echo -e "Removing artifacts...\n"
 rm -rf $DWNLD_DIR/{$BUNDLE_FILENAME,$CERT_FILENAME,$PKG_FILENAME}
@@ -51,4 +64,4 @@ echo -e "Done\n"
 FirefoxCertDir=$(dirname $(find $HOME/.mozilla -name cert*.db))
 echo "Firefox DB: $FirefoxCertDir"
 
-exit 0
+exit EXIT_SUCCESS
