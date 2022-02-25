9e9389befaf06afb87e068a15c64b34a6ad0fec2
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2022-02-25 11:55:57 -0500

    Adding current progress. I need to test the loops that import the certs and install libcackey.so.

--- cac_setup.sh
+++ cac_setup.sh
@@ -6,62 +6,89 @@
 # Date: 24 Feb 2022
 # Description: Setup a Linux environment for Common Access Card use.
 
-DWNLD_DIR=/tmp
-ROOT_UID=0      # Only users with $UID 0 have root privileges
-EXIT_SUCCESS=0
-EXIT_FAILURE=1
-CERT_FILENAME="AllCerts"
-BUNDLE_FILENAME="AllCerts.zip"
-CERT_URL="http://militarycac.com/maccerts/$BUNDLE_FILENAME"
-PKG_FILENAME="cackey_0.7.5-1_amd64.deb"
-CACKEY_URL="http://cackey.rkeene.org/download/0.7.5/$PKG_FILENAME"
+main ()
+{
+    EXIT_SUCCESS=0
+    EXIT_FAILURE=1
+    DWNLD_DIR=/tmp
+    ROOT_UID=0      # Only users with $UID 0 have root privileges
+    CERT_FILENAME="AllCerts"
+    BUNDLE_FILENAME="AllCerts.zip"
+    CERT_URL="http://militarycac.com/maccerts/$BUNDLE_FILENAME"
+    PKG_FILENAME="cackey_0.7.5-1_amd64.deb"
+    CACKEY_URL="http://cackey.rkeene.org/download/0.7.5/$PKG_FILENAME"
 
-# Ensure the script is ran as root
-# TODO uncomment
-#if [ "$UID" -ne "$ROOT_UID" ]
-#then
-    #echo "Please run this script as root."
-    #exit $E_NOTROOT
-#fi
+    # Ensure the script is ran as root
+    # TODO uncomment
+    #if [ "$UID" -ne "$ROOT_UID" ]
+    #then
+        #echo "Please run this script as root."
+        #exit $E_NOTROOT
+    #fi
 
-# Install middleware and necessary utilities
-# TODO uncomment
-#echo "Installing middleware..."
-#apt update
-#apt install -y libpcsclite1 pcscd libccid libpcsc-perl pcsc-tools wget unzip
-#echo "Done"
+    # Install middleware and necessary utilities
+    # TODO uncomment
+    #echo "Installing middleware..."
+    #apt update
+    #DEBIAN_FRONTEND=noninteractive apt install -y libpcsclite1 pcscd libccid libpcsc-perl pcsc-tools wget unzip
+    #echo "Done"
 
-# Pull all necessary files
-echo -e "Downloading DoD certificates and Cackey package...\n"
-wget -P $DWNLD_DIR $CERT_URL
-wget -P $DWNLD_DIR $CACKEY_URL
-echo -e "Done\n"
+    # Pull all necessary files
+    echo -e "Downloading DoD certificates and Cackey package...\n"
+    wget -P $DWNLD_DIR $CERT_URL
+    wget -P $DWNLD_DIR $CACKEY_URL
+    echo -e "Done\n"
 
-# Unzip cert bundle
-mkdir -p $DWNLD_DIR/$CERT_FILENAME
-unzip $DWNLD_DIR/$BUNDLE_FILENAME -d $DWNLD_DIR/$CERT_FILENAME
+    # Unzip cert bundle
+    mkdir -p $DWNLD_DIR/$CERT_FILENAME
+    unzip $DWNLD_DIR/$BUNDLE_FILENAME -d $DWNLD_DIR/$CERT_FILENAME
 
-# Check for Chrome
-_=google-chrome --version
-if [ $? -eq 0 ]
-then
-    # TODO do chrome loop here
-fi
+    # Check for Chrome
+    _=google-chrome --version
+    if [ $? -eq 0 ]
+    then
+        # Locate Firefox's database directory in the user's profile
+        ChromeCertDB=$(dirname $(find $HOME/.pki -name cert*.db))
+        if [ $? -eq 0 ]
+        then
+            # Import DoD certificates
+            echo "Importing DoD certificates for Chrome..."
+            for cert in $DWNLD_DIR/$CERT_FILENAME/*.cer
+            do
+                certutil -d sql:$ChromeCertDB -A -t TC -n $cert -i $cert
+            done
+            echo "Done."
+        else
+            echo "error: unable to find Chromes's certificate database"
+        fi
+    fi
 
-# Check for Firefox
-_=firefox --version
-if [ $? -eq 0 ]
-then
-    # TODO do firefox loop here
-fi
+    # Check for Firefox
+    _=firefox --version
+    if [ $? -eq 0 ]
+    then
+        # Locate Firefox's database directory in the user's profile
+        FirefoxCertDB=$(dirname $(find $HOME/.mozilla -name cert*.db))
+        if [ $? -eq 0 ]
+        then
+            # Import DoD certificates
+            echo "Importing DoD certificates for Firefox..."
+            for cert in $DWNLD_DIR/$CERT_FILENAME/*.cer
+            do
+                certutil -d sql:$FirefoxCertDB -A -t TC -n $cert -i $cert
+            done
+            echo "Done."
+        else
+            echo "error: unable to find Firefox's certificate database"
+        fi
+    fi
 
-# Removed artifacts
-echo -e "Removing artifacts...\n"
-rm -rf $DWNLD_DIR/{$BUNDLE_FILENAME,$CERT_FILENAME,$PKG_FILENAME}
-echo -e "Done\n"
+    # Remove artifacts
+    echo -e "Removing artifacts...\n"
+    rm -rf $DWNLD_DIR/{$BUNDLE_FILENAME,$CERT_FILENAME,$PKG_FILENAME}
+    echo -e "Done\n"
 
-# Locate Firefox's database directory in the user's profile
-FirefoxCertDir=$(dirname $(find $HOME/.mozilla -name cert*.db))
-echo "Firefox DB: $FirefoxCertDir"
+    exit EXIT_SUCCESS
+}
 
-exit EXIT_SUCCESS
+main
